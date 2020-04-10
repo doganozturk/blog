b45645937f2cb8421df11c5a718bd7f23eee575e
Doğan Öztürk <doganozturk2005@gmail.com>
2020-04-11 01:33:32 +0300

    Added normalize.css.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addLayoutAlias('main', 'layouts/main.pug');
     eleventyConfig.addPassthroughCopy('public');
+    eleventyConfig.addPassthroughCopy('node_modules/normalize.css');
 };

847828e05a7de0d303c3e163bad1d5bc69fecf5f
Doğan Öztürk <doganozturk2005@gmail.com>
2020-04-12 03:21:29 +0300

    Dark mode work.

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,4 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addLayoutAlias('main', 'layouts/main.pug');
     eleventyConfig.addLayoutAlias('post', 'layouts/post.pug');
     eleventyConfig.addPassthroughCopy('public');
-    eleventyConfig.addPassthroughCopy('node_modules/github-markdown-css');
 };
--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,1 +1,33 @@
-console.log('HELLO!');
+function themeControls() {
+    const root = document.documentElement;
+    const switches = root.querySelectorAll('.switch');
+    const themeMapping = {
+        'light': {
+            '--bg-color': '#ecf0f1',
+            '--text-color': '#34495e',
+            '--title-color': '#2c3e50',
+            '--misc-color': '#d35400',
+        },
+        'dark': {
+            '--bg-color': '#2c3e50',
+            '--text-color': '#ecf0f1',
+            '--title-color': '#bdc3c7',
+            '--misc-color': '#f39c12',
+        }
+    };
+
+    switches.forEach(item => {
+        item.addEventListener('click', e => {
+            const classList = item.classList;
+            const selectedTheme = classList[1].includes('light') ? 'light' : 'dark';
+
+            for (const property in themeMapping[selectedTheme]) {
+                if (themeMapping[selectedTheme].hasOwnProperty(property)) {
+                    root.style.setProperty(property, themeMapping[selectedTheme][property])
+                }
+            }
+        });
+    });
+}
+
+themeControls();

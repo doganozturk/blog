192fe13231cecfef0d775a8cbd2f53535725ab65
Doğan Öztürk <doganozturk2005@gmail.com>
2020-04-12 20:34:32 +0300

    Finishing touches on the design side.

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,6 +1,6 @@
 function themeControls() {
     const root = document.documentElement;
-    const switches = root.querySelectorAll('.switch');
+    const switches = root.querySelectorAll('.theme-switcher .switch');
     const themeMapping = {
         'light': {
             '--bg-color': '#ecf0f1',
@@ -15,11 +15,36 @@ function themeControls() {
             '--misc-color': '#f39c12',
         }
     };
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme && (savedTheme === 'light' || savedTheme === 'dark')) {
+        root.style.setProperty('--theme-name', savedTheme);
+
+        for (const property in themeMapping[savedTheme]) {
+            if (themeMapping[savedTheme].hasOwnProperty(property)) {
+                root.style.setProperty(property, themeMapping[savedTheme][property])
+            }
+        }
+
+        if (savedTheme === 'light') {
+            switches[0].classList.add('hidden');
+            switches[1].classList.remove('hidden');
+        }
+    }
 
     switches.forEach(item => {
         item.addEventListener('click', e => {
-            const classList = item.classList;
-            const selectedTheme = classList[1].includes('light') ? 'light' : 'dark';
+            const currentTheme = getComputedStyle(root, ':root')
+                .getPropertyValue('--theme-name');
+            const selectedTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+            switches.forEach(itm => {
+               itm.classList.toggle('hidden');
+            });
+
+            root.style.setProperty('--theme-name', selectedTheme);
+
+            localStorage.setItem('theme', selectedTheme);
 
             for (const property in themeMapping[selectedTheme]) {
                 if (themeMapping[selectedTheme].hasOwnProperty(property)) {

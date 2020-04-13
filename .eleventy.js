6ca1bba84669e95e396cb8a47d58817a30a463f6
Doğan Öztürk <doganozturk2005@gmail.com>
2020-04-14 00:14:48 +0300

    Changed hosting to Netlify.

--- .eleventy.js
+++ .eleventy.js
@@ -5,5 +5,4 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addLayoutAlias('main', 'layouts/main.pug');
     eleventyConfig.addLayoutAlias('post', 'layouts/post.pug');
     eleventyConfig.addPassthroughCopy('public');
-    eleventyConfig.addPassthroughCopy('CNAME');
 };

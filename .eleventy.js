721088c18d1801027214e602197aa02b2a0274db
Doğan Öztürk <doganozturk2005@gmail.com>
2020-04-13 18:04:17 +0300

    Fix CNAME for custom domain.

--- .eleventy.js
+++ .eleventy.js
@@ -5,4 +5,5 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addLayoutAlias('main', 'layouts/main.pug');
     eleventyConfig.addLayoutAlias('post', 'layouts/post.pug');
     eleventyConfig.addPassthroughCopy('public');
+    eleventyConfig.addPassthroughCopy('CNAME');
 };

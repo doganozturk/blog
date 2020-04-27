c6c98fdf8f0626db1942652b75c4b1305134792a
Doğan Öztürk <doganozturk2005@gmail.com>
2020-04-27 23:04:42 +0300

    Some performance & Lighthouse optimizations.

--- service-worker.js
+++ service-worker.js
@@ -2,16 +2,23 @@ importScripts('/node_modules/workbox-sw/build/workbox-sw.js');
 
 if (workbox) {
     workbox.routing.registerRoute(
-        /\.ttf$/,
+        /^https:\/\/fonts\.googleapis\.com/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'google-fonts-stylesheets',
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.gstatic\.com/,
         new workbox.strategies.CacheFirst({
-            cacheName: 'fonts',
+            cacheName: 'google-fonts-webfonts',
             plugins: [
                 new workbox.cacheableResponse.CacheableResponsePlugin({
                     statuses: [0, 200],
                 }),
                 new workbox.expiration.ExpirationPlugin({
                     maxAgeSeconds: 60 * 60 * 24 * 365,
-                    maxEntries: 5,
+                    maxEntries: 30,
                 }),
             ],
         })

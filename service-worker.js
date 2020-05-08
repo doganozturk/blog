28ad9ef45fc6a9550937a281005f2de7cd064f8c
Doğan Öztürk <doganozturk2005@gmail.com>
2020-05-08 15:11:30 +0300

    Change font-family on Post layout & header updates.

--- service-worker.js
+++ service-worker.js
@@ -2,22 +2,6 @@ importScripts('/node_modules/workbox-sw/build/workbox-sw.js');
 
 if (workbox) {
     workbox.routing.registerRoute(
-        /^https:\/\/fonts\.gstatic\.com/,
-        new workbox.strategies.CacheFirst({
-            cacheName: 'google-fonts-webfonts',
-            plugins: [
-                new workbox.cacheableResponse.CacheableResponsePlugin({
-                    statuses: [0, 200],
-                }),
-                new workbox.expiration.ExpirationPlugin({
-                    maxAgeSeconds: 60 * 60 * 24 * 365,
-                    maxEntries: 30,
-                }),
-            ],
-        })
-    );
-
-    workbox.routing.registerRoute(
         /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
         new workbox.strategies.CacheFirst({
             cacheName: 'images',
